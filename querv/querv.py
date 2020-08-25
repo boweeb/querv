91d3081b69c1dcecb9c4c8f6b8d8d81796a5c9bd
Jesse Butcher <boweeb@gmail.com>
2020-08-25 10:16:09 -0400

    Linting, added "save" cmd, better type hints, etc

--- querv/querv.py
+++ querv/querv.py
@@ -1,41 +1,62 @@
-#!/usr/bin/env python3.5
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 """querv
 
 Usage:
-  querv list [-p PROP] [-f FILE] [-i ID] [-m METHOD] [-a PROFILE]
+    querv list [-p PROP] [-f FILE] [-i ID] [-m METHOD] [-a PROFILE] [-r REGION]
+    querv save [-a PROFILE] [-r REGION]
 
-  querv -h | --help
-  querv --version
+    querv -h | --help
+    querv --version
 
 Any long-form option (id, file, profile, etc.) may also be specified as an environment variable of the form QUERV_$VAR
 where $VAR is the option in upper case.  Specifying an environment variable takes precedence of the CLI option.
 
+Use "save" to initialize an "output.json" file for high iteration.
+
 Options:
-  -h --help                  Show this screen.
-  --version                  Show version.
-  -p PROP --property=PROP    The property to pivot on.
-                             Currently implemented: "subnets", "images", "keys", "types" and "VPCs"[default: subnets]
-  -i ID --id=ID              Identify the ec2 instance by "id" or "tag" [default: id]
-  -m METHOD --method=METHOD  The method of retrieving the instances description [default: json_file]
-  -f FILE --file=FILE        Use an input file [default: output.json]
-  -a PROFILE --aws=PROFILE   The AWS profile to use for querying [default: default]
+    -h --help                         Show this screen.
+    --version                         Show version.
+    -p PROP --property=PROP           The property to pivot on.
+                                      Currently implemented: ["subnets", "images", "keys", "types", "VPCs"]
+                                      [default: subnets]
+    -i ID --id=ID                     Identify the ec2 instance by "id" or "tag" [default: id]
+    -m METHOD --method=METHOD         The method of retrieving the instances description [default: json_file]
+    -f FILE --file=FILE               Use an input file [default: output.json]
+    -a PROFILE --aws-profile=PROFILE  The AWS profile to use for querying [default: default]
+    -r REGION --aws-region=REGION     The AWS profile to use for querying [default: us-west-2]
+
+TODO:
+    - ... so much that the main "todo" is to create this list.  There's lots of room for improvement, here.
 """
 
-from __future__ import unicode_literals, print_function
 
-import os
+import datetime
 import json
-from docopt import docopt
-from typing import Tuple
+import os
+import sys
+from importlib.metadata import version
+from typing import Dict, List, Text, Tuple, Union
+
 import boto3
-
-__author__ = 'Jesse Butcher'
-__email__ = 'boweeb@gmail.com'
-__version__ = '0.1.0'
+from docopt import docopt
 
 
-def get_option(opt: str, args: dict) -> str:
+class DateTimeEncoder(json.JSONEncoder):
+    # Override the default method
+    def default(self, obj):
+        if isinstance(obj, (datetime.date, datetime.datetime)):
+            return obj.isoformat()
+
+
+def save_snapshot(args):
+    args["--method"] = "live"
+    raw_data = get_data(args, trim=False)
+    with open("output.json", "+w") as f:
+        f.write(json.dumps(raw_data, indent=4, cls=DateTimeEncoder))
+    print('WARNING: You _might_ have to find output.json under ".venv/bin"')
+
+
+def get_option(opt: Text, args: Dict) -> Text:
     """
     Args:
         opt:
@@ -44,11 +65,11 @@ def get_option(opt: str, args: dict) -> str:
     Returns:
 
     """
-    opt_env = 'QUERV_{}'.format(opt.upper())
+    opt_env = "QUERV_{}".format(opt.upper())
     if opt_env in os.environ.keys():
         return os.environ[opt_env]
     else:
-        return args['--{}'.format(opt)]
+        return args["--{}".format(opt)]
 
 
 # def print_result_as_list(data):
@@ -56,61 +77,64 @@ def get_option(opt: str, args: dict) -> str:
 #     print("------------+------------")
 
 
-def get_data(args: dict) -> list:
+def get_data(args: Dict, trim: bool = True) -> Union[Dict, List]:
     """
     Args:
-        args (dict):
+        args:
+        trim: Automatically descend into first level, "Reservations" (because it is known).
 
     Returns:
-        list: asdf
+        Foobar
 
     Raises:
         ValueError: blah
     """
-    method = get_option('method', args)
-    """str: Method
-    """
+    method: Text = get_option("method", args)
 
-    if method == 'json_file':
-        in_file = get_option('file', args)
+    if method == "json_file":
+        in_file = get_option("file", args)
         with open(in_file) as f:
             raw_data = json.load(f)
-    elif method == 'live':
-        aws_prof = get_option('aws', args)
-        boto3.setup_default_session(profile_name=aws_prof)
-        client = boto3.client('ec2')
+    elif method == "live":
+        aws_profile = get_option("aws-profile", args)
+        aws_region = get_option("aws-region", args)
+        boto3.setup_default_session(profile_name=aws_profile, region_name=aws_region)
+        client = boto3.client("ec2")
         raw_data = client.describe_instances()
     else:
         raise ValueError("Invalid get_data method")
 
-    # All data is inside the "Reservations" key
-    return raw_data['Reservations']
+    if trim:
+        # All data is inside the "Reservations" key
+        return raw_data["Reservations"]
+    else:
+        return raw_data
 
 
-def get_summary(data: list, ident: str, query: str) -> Tuple[dict, list]:
+def get_summary(data: List, ident: Text, query: Text) -> Tuple[Dict, List]:
     """Get summary
 
     Args:
-        data (list): input data
-        ident (str): y
-        query (str): z
+        data: input data
+        ident: y
+        query: z
 
     Returns:
-        Tuple[dict, list]: A tuple containing a dictionary and a list.
+        A tuple containing a dictionary and a list.
 
     """
     summary_dict = {}
     for obj in data:
-        instance = obj['Instances'][0]  # 'Instances' is always a list with a length of 1
+        instance = obj["Instances"][0]  # 'Instances' is always a list with a length of 1
 
-        fallback = instance['InstanceId']
-        if ident == 'id':
-            ec2_id = instance['InstanceId']
-        elif ident == 'tag':
+        fallback = instance["InstanceId"]
+        if ident == "id":
+            ec2_id = instance["InstanceId"]
+        elif ident == "tag":
             results = []
-            for key in instance['Tags']:
-                if key['Key'] == 'Name':
-                    results.append(key['Value'])
+            for key in instance["Tags"]:
+                if key["Key"] == "Name":
+                    results.append(key["Value"])
             if len(results) != 1:
                 ec2_id = fallback
             else:
@@ -131,7 +155,7 @@ def get_summary(data: list, ident: str, query: str) -> Tuple[dict, list]:
     return summary_dict, summary_list
 
 
-def pivot(summary_dict: dict, summary_list: list) -> str:
+def pivot(summary_dict: Dict, summary_list: List) -> Text:
     """
     Args:
         summary_dict:
@@ -141,32 +165,37 @@ def pivot(summary_dict: dict, summary_list: list) -> str:
 
     """
     s = set(val for dic in summary_list for val in dic.values())
-    uniqs = {}
+    unique_map = {}
     for x in s:
-        if x not in uniqs.keys():
-            uniqs[x] = []
+        if x not in unique_map.keys():
+            unique_map[x] = []
         for k, v in summary_dict.items():
             if x == v:
-                uniqs[x].append(k)
+                unique_map[x].append(k)
 
-    return json.dumps(uniqs, indent=4, sort_keys=True)
+    return json.dumps(unique_map, indent=4, sort_keys=True)
 
 
 def main():
     """Main entry point for the querv CLI.
     """
-    args = docopt(__doc__, version=__version__)
+    args = docopt(__doc__, version=version("querv"))
     # print(args)
 
-    prop_dict = {'subnets': 'SubnetId',
-                 'images': 'ImageId',
-                 'keys': 'KeyName',
-                 'types': 'InstanceType',
-                 'VPCs': 'VpcId',
-                 }
-    query_input = get_option('property', args)
+    prop_dict = {
+        "subnets": "SubnetId",
+        "images": "ImageId",
+        "keys": "KeyName",
+        "types": "InstanceType",
+        "VPCs": "VpcId",
+    }
+    query_input = get_option("property", args)
     query = prop_dict[query_input]
-    ident = get_option('id', args)
+    ident = get_option("id", args)
+
+    if args["save"]:
+        save_snapshot(args)
+        sys.exit(0)
 
     data = get_data(args)
     summary_dict, summary_list = get_summary(data, ident, query)
@@ -174,8 +203,5 @@ def main():
     print(pivot(summary_dict, summary_list))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-
-# vim:fileencoding=utf-8
