50da22a1650ef2a7d7bebf2126770b334000c65b
Jesse Butcher <boweeb@gmail.com>
2023-10-22 10:14:53 -0400

    Bump deps

--- src/querv/__init__.py
+++ src/querv/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-__author__ = 'Jesse Butcher'
-__email__ = 'boweeb@gmail.com'
-__version__ = '0.2.0'
+__author__ = "Jesse Butcher"
+__email__ = "boweeb@gmail.com"
+__version__ = "0.3.0"
